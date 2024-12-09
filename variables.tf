44c29b18e2e4614ff19978f6574f9e85ab95f14e
Vijeth Shetty <vijeth.shetty@gmail.com>
2024-12-09 14:23:36 -0800

    Added AWS  Access keya and Secret key environment variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID for authenticating with AWS"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key for authenticating with AWS"
+  type        = string
+}
